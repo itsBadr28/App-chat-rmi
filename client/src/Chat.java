8f053c83456edd8a4c164422957285fce16cbe52
itsBadr <adbadr28@gmail.com>
2022-11-27 05:18:29 +0100

    delete file

--- client/src/Chat.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Chat extends Remote {
-
-    public String chat(String username ,String message)throws RemoteException;
-}
-
